e35afe90aa281c54d6c5ab7555d7059b30808f08
Tuan Khang <you@example.com>
2021-09-15 11:27:15 +0700

    employee crud

--- src/main/java/sv/iuh/springbootapi/repository/EmployeeRepository.java
+++ src/main/java/sv/iuh/springbootapi/repository/EmployeeRepository.java
@@ -1,7 +1,9 @@
 package sv.iuh.springbootapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import sv.iuh.springbootapi.entity.Employee;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
 }
